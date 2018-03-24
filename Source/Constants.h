1004b9fe1b980bb9b1dca1aa5161e77b6c6d494d
pauliuskuzmickas <p.kuzmickas@gmail.com>
2018-03-24 18:34:23 +0000

    Created the base for the game

---
+++ Source/Constants.h
@@ -0,0 +1,45 @@
+#pragma once
+
+/*
+Class for holding the constant global values for the game
+The names should be self explanatory and if not, should be changed to be so
+*/
+
+#define SIN45 0.707f
+#define ASSET_DIR "Assets/"
+
+class Constants {
+
+public:
+
+	static const int SCREEN_WIDTH = 1000;
+	static const int SCREEN_HEIGHT = 800;
+	static const int TILE_SIZE = 32;
+	static const int ANIMSPEED = 250;
+	static const int COL_UP_ERROR = 5;
+	static const int COL_DOWN_ERROR = 5;
+	static const int COL_LEFT_ERROR = 5;
+	static const int COL_RIGHT_ERROR = 5;
+	static const int MAX_LAYERS = 5;
+	static const int ATTACK_DIST = 15;
+	static const int DASHING_DIST = 400;
+	static const int SLASH_SPEED = 25; //the lower the faster
+	static const int KUNAI_ANIM_SPEED = 25; //the lower the faster
+											//static const int KUNAI_SPEED = 1; //the higher the faster
+	static const int INDICATOR_WIDTH = 32;
+	static const int INDICATOR_HEIGHT = 160;
+	static const int SLOW_MOTION_SPEED = 25;
+	static const int DEFAULT_PLAYER_SPEED = 200;
+	static const int CONSOLE_FONT_SIZE = 28; // 28
+	static const int DIALOGUE_LETTER_ANIM_TIME = 80; // milliseconds
+	static const int DIALOGUE_LETTER_ANIM_TIME_FAST = 5; // milliseconds
+
+	static const int KUNAI_SPEED = 1;
+	static const int MAIN_ATT_CD = 0;
+	static const int SECOND_ATT_CD = 0;
+	static const int SPECIAL_ATT_CD = 2;
+	static const int DASH_MOVE_CD = 0;
+	
+
+};
+
---
+++ Source/main.cpp
@@ -0,0 +1,30 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include "Constants.h"
+using namespace std;
+
+/*
+Initializes the game, creates a window and runs the game.
+The entry point of the program.
+*/
+
+int main(int argc, char* args[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	if (TTF_Init() == -1)
+		cout << "TTF NOT INIT" << endl;
+	SDL_StopTextInput(); // Because previously it would be on by default for some reason
+	SDL_Window* gWindow = SDL_CreateWindow("Homecoming", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, Constants::SCREEN_WIDTH, Constants::SCREEN_HEIGHT, SDL_WINDOW_SHOWN /*|| SDL_WINDOW_FULLSCREEN*/);
+	SDL_Renderer* gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+	// Sets the draw color (in case there will be any GUI drawing with SDL)
+	SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 0xFF);
+	//Director director(gRenderer);
+	//director.StartGame();
+
+
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	SDL_Quit();
+	return 0;
+}
