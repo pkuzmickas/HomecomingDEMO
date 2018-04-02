463cf4a544354ce8df9a69958fa489fa6de98c02
pauliuskuzmickas <p.kuzmickas@gmail.com>
2018-04-02 21:26:03 +0100

    Rewrote the map system to get 30 fps. Not enough. Thought of a new system that this needs to be rewritten to. (2D maps sound most efficient)

--- Source/Graphics.cpp
+++ Source/Graphics.cpp
@@ -5,26 +5,27 @@ void Graphics::addToDraw(Entity * entity) {
 	if (!drawableComponent) {
 		std::cout << "Entity does not have a drawable component! Did not add to the draw queue." << std::endl;
 		return;
-	} 
+	}
 	objectDrawQueue[drawableComponent->layer].push_back(entity);
 }
 
 void Graphics::addMap(std::vector<std::vector<std::vector<Entity*>>> mapMatrix) {
-	
-	for (int i = 0; i < Globals::Layers::END_MARKER; i++) {
-		std::vector<std::vector<Entity*>> layer;
-		mapDrawQueue.push_back(layer);
-	}
 
 	for (int i = 0; i < mapMatrix.size(); i++) {
-		std::vector<Entity*> row;
+		std::vector<std::map<Globals::Layers, Entity*>> row;
+		for (int j = 0; j < mapMatrix[i].size(); j++) {
+			std::map<Globals::Layers, Entity*> col;
+			row.push_back(col);
+		}
+		mapDrawQueue.push_back(row);
+	}
+	
+	for (int i = 0; i < mapMatrix.size(); i++) {
 		for (int j = 0; j < mapMatrix[i].size(); j++) {
 			for (auto entity : mapMatrix[i][j]) {
 				Drawable* curDrawable = (Drawable*)entity->findComponent(ComponentType::DRAWABLE);
-				mapDrawQueue[curDrawable->layer][i][j] = entity;
-				// ^ TEST IF ABOVE WORKS //TODO
+				mapDrawQueue[i][j][curDrawable->layer] = entity;
 			}
-
 		}
 	}
 
@@ -44,7 +45,7 @@ void Graphics::render()
 
 	// RENDERS MAP
 
-	renderMap();
+	if(!mapDrawQueue.empty()) renderMap();
 
 	// RENDERS GAME OBJECTS
 
@@ -54,27 +55,21 @@ void Graphics::render()
 }
 
 void Graphics::renderObjects() {
-	int numOfRows = (int)objectDrawQueue.size();
-	for (int i = 0; i < numOfRows; i++) {
-		int numOfColumns = (int)objectDrawQueue[i].size();
-		for (int j = 0; j < numOfColumns; j++) {
-			
+	for (int i = 0; i < (int)objectDrawQueue.size(); i++) {
+		for (int j = 0; j < (int)objectDrawQueue[i].size(); j++) {
+
 			Entity* curEntity = objectDrawQueue[i][j];
-			Drawable* curDrawable = (Drawable*)objectDrawQueue[i][j]->findComponent(ComponentType::DRAWABLE);
-			Transform* curTransform = (Transform*)objectDrawQueue[i][j]->findComponent(ComponentType::TRANSFORM);
-			
+			Drawable* curDrawable = (Drawable*)curEntity->findComponent(ComponentType::DRAWABLE);
+			Transform* curTransform = (Transform*)curEntity->findComponent(ComponentType::TRANSFORM);
+
 			// Checks whether something is outside the camera and does not draw it
 			if (curTransform->globalPosX + Globals::TILE_SIZE < Camera::posX || curTransform->globalPosY + Globals::TILE_SIZE < Camera::posY || curTransform->globalPosX > Camera::posX + Globals::SCREEN_WIDTH || curTransform->globalPosY > Camera::posY + Globals::SCREEN_HEIGHT) continue;
-			
-			
 
 			SDL_Rect localPos;
 			localPos.h = curTransform->height;
 			localPos.w = curTransform->width;
 			localPos.x = (int)(curTransform->globalPosX - Camera::posX);
 			localPos.y = (int)(curTransform->globalPosY - Camera::posY);
-			
-			
 
 			if (curTransform->isRotated) {
 				SDL_RenderCopyEx(renderer, curDrawable->image, curDrawable->srcRect, &localPos, curTransform->rotationAngle, &curTransform->rotationCenter, curDrawable->flip);
@@ -82,8 +77,6 @@ void Graphics::renderObjects() {
 			else {
 				SDL_RenderCopy(renderer, curDrawable->image, curDrawable->srcRect, &localPos);
 			}
-
-			
 
 			if (debug) {
 				if (curEntity->hasComponent(ComponentType::COLLIDER)) {
@@ -104,5 +97,44 @@ void Graphics::renderObjects() {
 }
 
 void Graphics::renderMap() {
+	for (int layer = 0; layer < Globals::Layers::UI; layer++) {
+		for (int i = Camera::posY / Globals::TILE_SIZE; i < Camera::posY / Globals::TILE_SIZE + Globals::SCREEN_HEIGHT / Globals::TILE_SIZE + 1; i++) {
+			if (i >= mapDrawQueue.size()) break;
+			for (int j = Camera::posX / Globals::TILE_SIZE; j < Camera::posX / Globals::TILE_SIZE + Globals::SCREEN_WIDTH / Globals::TILE_SIZE + 1; j++) {
+				if (j >= mapDrawQueue[i].size()) break;
+				if(mapDrawQueue[i][j].find((Globals::Layers)layer) != mapDrawQueue[i][j].end()) {
 
+					Entity* curEntity = mapDrawQueue[i][j][(Globals::Layers)layer];
+					if (curEntity == NULL) {
+						continue;
+					}
+
+					Drawable* curDrawable = (Drawable*)curEntity->findComponent(ComponentType::DRAWABLE);
+					Transform* curTransform = (Transform*)curEntity->findComponent(ComponentType::TRANSFORM);
+
+					SDL_Rect localPos;
+					localPos.h = curTransform->height;
+					localPos.w = curTransform->width;
+					localPos.x = (int)(curTransform->globalPosX - Camera::posX);
+					localPos.y = (int)(curTransform->globalPosY - Camera::posY);
+
+					SDL_RenderCopy(renderer, curDrawable->image, curDrawable->srcRect, &localPos);
+
+					if (debug) {
+						if (curEntity->hasComponent(ComponentType::COLLIDER)) {
+							Collider* col = (Collider*)curEntity->findComponent(ComponentType::COLLIDER);
+							SDL_Rect collider;
+							collider.h = col->colBox.h;
+							collider.w = col->colBox.w;
+							collider.x = col->colBox.x;
+							collider.y = col->colBox.y;
+							SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0xFF);
+							SDL_RenderDrawRect(renderer, &collider);
+							SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- Source/Graphics.h
+++ Source/Graphics.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <iterator>
+#include <map>
 class Graphics {
 public:
 	void addToDraw(Entity* entity); // for game objects
@@ -12,7 +13,7 @@ public:
 	void render();
 private:
 	// Map is drawn in a separate way to increase performance
-	std::vector<std::vector<std::vector<Entity*>>> mapDrawQueue; // [LAYER] [ROW] [COL]
+	std::vector<std::vector<std::map<Globals::Layers, Entity*>>> mapDrawQueue; // [ROW] [COL] [LAYER]
 	std::vector<std::vector<Entity*>> objectDrawQueue; // [LAYER] [ENTITY]
 	SDL_Renderer* renderer;
 	void renderObjects();
